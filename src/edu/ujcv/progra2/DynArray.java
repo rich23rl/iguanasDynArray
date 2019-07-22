a5609ac83a3e6b6c6b7848084f6fa608dda25ce4
kenneth vittetoe <kenneth.vittetoe@ujcv.edu.hn>
2019-07-22 08:18:58 -0600

    Initial commit

---
+++ src/edu/ujcv/progra2/DynArray.java
@@ -0,0 +1,90 @@
+package edu.ujcv.progra2;
+
+public class DynArray {
+
+    private int[] array;
+    private int size;
+
+    public DynArray(){
+
+        array = new int[10];
+        size = 0;
+    }
+
+    public DynArray(int size){
+        array = new int[size];
+        this.size = 0;
+    }
+
+
+    // array[n] = 3;
+
+    public void setIndex(int n,int element){
+        if(n > size){
+            throw new IndexOutOfBoundsException();
+        }
+        array[n] = element;
+    }
+
+
+    // x = array[n];
+
+    public int getIndex(int n){
+        if(n > size){
+            throw new IndexOutOfBoundsException();
+        }
+        return array[n];
+    }
+
+    public void add(int element){
+        if(size >= array.length){
+                grow();
+        }
+        array[size++] = element;
+    }
+
+    private void grow() {
+
+
+        int[] temp = new int[array.length * 2];
+
+        System.arraycopy(array,0,temp,0,array.length);
+
+        //for (int i = 0; i < array.length; i++) {
+        //  temp[i]= array[i];
+        //}
+
+        array = temp;
+    }
+
+    public void remove(){
+        size--;
+    }
+
+
+
+
+    public String toString(){
+       StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < array.length; i++) {
+            if(i< size){
+                builder.append(array[i]);
+                builder.append(',');
+                builder.append(' ');
+            }else{
+                builder.append('v');
+                builder.append(',');
+                builder.append(' ');
+
+            }
+        }
+        int sizeb = builder.length();
+
+        builder.deleteCharAt(sizeb - 1);
+        builder.deleteCharAt(sizeb - 2);
+
+        return builder.toString();
+
+    }
+}
---
+++ src/edu/ujcv/progra2/Main.java
@@ -0,0 +1,23 @@
+package edu.ujcv.progra2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        DynArray areglo = new DynArray(5);
+
+        areglo.add(1);
+        areglo.add(2);
+        areglo.add(3);
+        areglo.add(4);
+        areglo.add(5);
+        areglo.add(6);
+        areglo.add(7);
+        areglo.add(8);
+
+
+
+    }
+}
